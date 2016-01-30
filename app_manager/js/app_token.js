301292d71b3f1ebcb4a898653a127b205869d77a
Sopheak Both <sopheakboth83@gmail.com>
2016-01-30 21:36:18 +0100

    $.getJSON() instead of FB.api()

--- app_manager/js/app_token.js
+++ app_manager/js/app_token.js
@@ -1,12 +1,19 @@
 function getAppToken(clientId, clientSecret) {
-FB.api(
-  '/oauth/access_token?client_id=' + clientId + '&client_secret=' + clientSecret + '&grant_type=client_credentials',
-  'get',
-  function(response) {
-    // handle the response
-    console.log('app_token:');
-    console.log(response);
-  }
-);
+//FB.api(
+//  '/oauth/access_token?client_id=' + clientId + '&client_secret=' + clientSecret + '&grant_type=client_credentials',
+//  'get',
+//  function(response) {
+//    // handle the response
+//    console.log('app_token:');
+//    console.log(response);
+//  }
+//);
 
+$.getJSON('https://graph.facebook.com/v2.5/oauth/access_token', {
+    client_id: clientId,
+    client_secret: clientSecret
+})
+.done(function(data) {
+    console.log(data);
+});
 }
