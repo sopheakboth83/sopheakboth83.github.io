956d74f1f8edc5d47ff562be56050df74583f5c3
Sopheak Both <sopheakboth83@gmail.com>
2016-01-30 21:02:08 +0100

    obtain app token getAppToken

--- app_manager/js/app_token.js
+++ app_manager/js/app_token.js
@@ -1,4 +1,4 @@
-function(clientId, clientSecret) {
+function getAppToken(clientId, clientSecret) {
 FB.api(
   '/oauth/access_token?client_id=' + clientId + '&client_secret=' + clientSecret + '&grant_type=client_credentials',
   'get',
