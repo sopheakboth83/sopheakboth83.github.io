6a9e661d0abbffa58f1dc52eb08dc412ebdc2193
Sopheak Both <sopheakboth83@gmail.com>
2016-01-30 20:58:51 +0100

    obtain app token

---
+++ app_manager/js/app_token.js
@@ -0,0 +1,11 @@
+function(clientId, clientSecret) {
+FB.api(
+  '/oauth/access_token?client_id=' + clientId + '&client_secret=' + clientSecret + '&grant_type=client_credentials',
+  'get',
+  function(response) {
+    // handle the response
+    console.log('app_token: ' + response);
+  }
+);
+
+}
